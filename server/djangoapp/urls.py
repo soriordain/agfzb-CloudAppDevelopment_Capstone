18cf2b33e70c78a609400b45f0e335f2685e59e2
S O'Riordain <soriordain@gmail.com>
2022-11-08 06:42:21 -0500

    Added login view

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # path for registration
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,8 +31,20 @@ def contact(request):
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
