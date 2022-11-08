5e3221cdfcbd93de7f3f37de12eb9a16258d36ee
S O'Riordain <soriordain@gmail.com>
2022-11-08 05:32:00 -0500

    Lab 1 updates

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "sor-bestcars.eu-de.cf.appdomain.cloud",
+]
 
 
 # Application definition
